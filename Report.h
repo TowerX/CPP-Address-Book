c9f4c1770669b021a10aa83d559882a9be510303
TowerX <jptb86@gmail.com>
2020-10-10 12:42:29 -0400

    Add: Setup private member variables

--- Report.h
+++ Report.h
@@ -1,10 +1,24 @@
 /*  Record Class Declaration File
 	Record.h
 */
+#include <cstdlib>;
+#include <iostream>;
+#include <string>;
+
+using namespace std; // Bad practice
+
 #pragma once
 #ifndef REPORT_H
 #define REPORT_H
 class Report
 {
+private:
+	string recordID;
+	string firstname;
+	string lastName;
+	string age;
+	string telephone;
+public:
+
 };
 #endif
